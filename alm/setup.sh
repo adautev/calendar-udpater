413a5f53c08f6aa560b37931b24e23f61f27dd61
Andrey Dautev <andrey.dautev@saorsa.bg>
2018-06-14 12:02:47 +0300

    Adding details for provisioning.

--- alm/setup.sh
+++ alm/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 git clone https://github.com/adautev/calendar-updater.git
 cd calendar-updater/
+mkdir credentials
 virtualenv venv
 source venv/bin/activate
 pip install -r virtualenv.txt
